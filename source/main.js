e2f30d2a7f1005a3cfbba440c84540c8a9533af0
J.Fukaya <fukajun.shark@gmail.com>
2016-03-13 19:10:23 +0900

    Enable copy and paste

--- source/main.js
+++ source/main.js
@@ -7,6 +7,7 @@ const INACTIVE_MENUBAR_ICON = __dirname + '/images/inactive.png'
 const NOTIFY_ICON           = __dirname + '/images/notify_icon.png'
 import menubar from 'menubar';
 import { app, ipcMain } from 'electron';
+import Menu from 'menu';
 import notifier from 'node-notifier';
 import path from 'path'
 
@@ -22,7 +23,28 @@ const switchIconRead = ()=> {
 const setTrayTitle = (title)=> {
   mb.tray.setTitle(title)
 }
+const initMenu = ()=> {
+  var template = [{
+      label: "Application",
+      submenu: [
+          { label: "About Application", selector: "orderFrontStandardAboutPanel:" },
+          { type: "separator" },
+          { label: "Quit", accelerator: "Command+Q", click: function() { app.quit(); }}
+      ]}, {
+      label: "Edit",
+      submenu: [
+          { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+          { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+          { type: "separator" },
+          { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+          { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+          { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+          { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+      ]}
+  ];
 
+  Menu.setApplicationMenu(Menu.buildFromTemplate(template));
+}
 
 mb.on('ready', function ready () {
 
@@ -63,5 +85,6 @@ mb.on('ready', function ready () {
 
   mb.showWindow();
   mb.hideWindow();
+  initMenu();
   switchIconUnread();
 })
