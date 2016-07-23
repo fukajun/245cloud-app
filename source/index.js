5c34f54492b770d413badd4e154bc0a75965c98f
J.Fukaya <fukajun.shark@gmail.com>
2016-07-23 11:55:45 +0900

    Fix typo

--- source/index.js
+++ source/index.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", ()=> {
     }
     ipcRenderer.send('quit')
   })
-  quitIcon.addEventListener('click', ()=> {
+  reloadIcon.addEventListener('click', ()=> {
     if(!confirm('リロードしますか？')) {
       return
     }
